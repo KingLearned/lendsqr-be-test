392979e07a4d6d8570cb04d260b7b3f08f5a4a0a
kinglearned <kinglearned@gmail.com>
2025-07-29 21:52:23 +0100

    Removed exposed env variables

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,12 @@
 import type { Knex } from "knex";
 import dotenv from "dotenv";
-import { CLIENT, DB_URL } from "./src/config/env";
+import { DB_URL } from "./src/config/env";
 
 dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: CLIENT,
+    client: "mysql2",
     connection: DB_URL,
     migrations: {
       directory: "./src/db/migrations"
